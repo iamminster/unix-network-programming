217c0e37c184ea017e827adcd557634dfbd2844f
nguyenminh <minhnguyen@abc.com>
2020-02-20 09:28:46 +0900

    format fork, pipe, signal examples

--- pipe_exam.c
+++ pipe_exam.c
@@ -4,19 +4,19 @@
 #include <unistd.h>
 
 int main(void) {
-	int pfds[2];
-	char buf[30];
-	
-	if (pipe(pfds) == -1) {
-		perror("pipe");
-		exit(1);
-	}
+    int pfds[2];
+    char buf[30];
 
-	printf("Writing to file descriptor #%d\n", pfds[1]);
-	write(pfds[1], "test", 5);
-	printf("Reading from file descriptor #%d\n", pfds[0]);
-	read(pfds[0], buf, 5);
-	printf("read \"%s\"\n", buf);
-	
-	return 0;
+    if (pipe(pfds) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    printf("Writing to file descriptor #%d\n", pfds[1]);
+    write(pfds[1], "test", 5);
+    printf("Reading from file descriptor #%d\n", pfds[0]);
+    read(pfds[0], buf, 5);
+    printf("read \"%s\"\n", buf);
+
+    return 0;
 }
